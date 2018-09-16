d3593ada2558d9f080afb079fa612cfbe534d3e4
gilles-degols <gilles@degols.net>
2018-09-16 20:27:06 +0200

    Better handling of configuration files

---
+++ README.md
@@ -0,0 +1,3 @@
+sbt "run -Dconfig.resource=conf/node0.conf 9000"
+sbt "run -Dconfig.resource=conf/node1.conf 9001"
+sbt "run -Dconfig.resource=conf/node2.conf 9002"
--- app/net/degols/example/election/Worker.scala
+++ app/net/degols/example/election/Worker.scala
@@ -1,14 +1,17 @@
 package net.degols.example.election
 
+import java.io.File
+
 import akka.actor.Actor
 import net.degols.filesgate.libs.election.{ConfigurationService, ElectionService, ElectionWrapper}
 import org.slf4j.LoggerFactory
 import javax.inject.{Inject, Singleton}
 
+import com.typesafe.config.{Config, ConfigFactory}
+
 @Singleton
 class Worker @Inject()(electionService: ElectionService, configurationService: ConfigurationService) extends ElectionWrapper(electionService, configurationService){
   private val logger = LoggerFactory.getLogger(getClass)
-
   override def receive: Receive = {
     case message =>
       logger.debug(s"[Worker] Received unknown message: $message")
