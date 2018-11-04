22cf035348de108865c999e0d605babbd5c68672
gilles-degols <gilles@degols.net>
2018-11-04 19:46:16 +0100

    Fix issues with basic dependencies

--- app/Module.scala
+++ app/Module.scala
@@ -1,4 +1,5 @@
 import com.google.inject.AbstractModule
+import net.degols.example.election.Worker
 import play.api.libs.concurrent.AkkaGuiceSupport
 
 class Module extends AbstractModule with AkkaGuiceSupport {
